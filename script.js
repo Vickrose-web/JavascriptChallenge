c5f4a561ef045be9f1266d4c2c872797425faee7
vickrose-web <victorsimon200@gmail.com>
2024-09-25 15:26:10 +0100

    javascript and javascript challenges

---
+++ script.js
@@ -0,0 +1,86 @@
+//displaying output
+// document.getElementById("demo").innerHTML = 5 + 6;
+
+// var x=2;
+// let y = 1;
+// const str = 'Java';
+
+// let num = 3;
+
+// let toastingMachine = function woo(){
+
+// }
+// {
+
+// }
+
+// let x = 10; 
+// // Assign the value 5 to x
+
+// let x = 5;
+// Assign the value 2 to y
+// let y = 2;
+// Assign the value x + y to z:
+// let z = x + y;
+// let z = x * y;
+// let a = 3;
+// let x =  a * (100 + 57);
+// let  y = 5;
+//remainder operator
+// let z = x % y;
+// console.log(z);
+
+//comparison operator
+// let text1 = "A";
+// let text2 = "B";
+// let result = text1 > text2;
+// console.log(result);
+
+//string comparison
+
+// let text1 = "John";
+// let text2 = "Doe";
+// let text3 = text1 + " " + text2;
+// console.log(text3);
+
+let num5 = BigInt("123456789012345678901234567890");
+// console.log(num5);
+let x = 5 + 5;
+let y = "5" + 5; //'fgsfgs'
+let z = "Hello" + 5;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+let num1 = 1;
+let num2 = "8";
+let num3 = 7;
+// 0, null, undefined, '', [], {}
+//logical and
+//it will return the first falsy value
+// console.log(num1 && num2);
+//logical not
+// console.log(!num1);
+//logical or
+//truthy and falsy value true or the first truthy value
+// console.log(num1 || num2);
+let obj = {};
+let arr = [];
+let arr1 = new Array();
+let str = 16 + "Volvo"; 
+// console.log(typeof arr1);
+
+// console.log(typeof num1 || num2);
+// console.log(typeof num1);
+// console.log(typeof num2);
+// console.log(typeof obj);
+// console.log(typeof arr);
+
+//array data types
+const cars = ["Saab", "Volvo", "BMW"];
+// console.log(cars[0]);
+
+//testing
+let car = "";  
+console.log( typeof car);
+// console.log(caR);
